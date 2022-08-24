e173d36972c0586cfa0db4c2359b013ef759ced6
EC2 Default User <ec2-user@ip-172-31-11-38.us-west-2.compute.internal>
2022-08-24 22:49:34 +0000

    Setting up EKS Blueprints deployment pipeline

---
+++ test/eks_blueprints_pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EksBlueprintsPipeline from '../lib/eks_blueprints_pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eks_blueprints_pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EksBlueprintsPipeline.EksBlueprintsPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
