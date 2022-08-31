f044487fb98dcfb1157cfce717ad35d9bec33cee
EC2 Default User <ec2-user@ip-172-31-11-38.us-west-2.compute.internal>
2022-08-31 22:36:43 +0000

    added ECR and DynamoDB Resources

--- bin/eks_blueprints_pipeline.ts
+++ bin/eks_blueprints_pipeline.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import ClusterConstruct from '../lib/eks_blueprints_pipeline-stack';
 import PipelineConstruct from '../lib/pipeline';
+import ServiceInfraStack from '../lib/service_infra_stack';
 
 
 const app = new cdk.App();
@@ -10,4 +11,5 @@ const region = process.env.CDK_DEFAULT_REGION;
 const env = { account, region }
 
 new ClusterConstruct(app, 'cluster', { env });
-new PipelineConstruct(app, 'pipeline', {env});
+new PipelineConstruct(app, 'pipeline', {env });
+new ServiceInfraStack(app, 'serviceInfra', { env });
---
+++ lib/service_infra_stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export default class ServiceInfraStack extends Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps){
+    super(scope, id)
+
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
+
+    const table = new dynamodb.Table(this, 'Posts', {
+        partitionKey: {
+            name: 'post_id',
+            type: dynamodb.AttributeType.NUMBER
+        },
+    })
+    
+    const repository = new ecr.Repository(this, 'Repo', {
+      repositoryName: "capstone",
+      imageScanOnPush: true,
+    });
+  }
+}
