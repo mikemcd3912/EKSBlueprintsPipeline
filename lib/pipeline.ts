aad274ed50692b4efdf02b4d78b2f39738865bf8
EC2 Default User <ec2-user@ip-172-31-11-38.us-west-2.compute.internal>
2022-09-02 17:20:03 +0000

    Adding the ArgoCD Addon to clone command to fix bootstrap ops

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -62,9 +62,9 @@ export default class PipelineConstruct extends Construct {
       .wave({
           id: "envs",
           stages: [
-              { id: "dev", stackBuilder: blueprint.clone('us-west-2')},
-              { id: "test", stackBuilder: blueprint.clone('us-east-2')},
-              { id: "prod", stackBuilder: blueprint.clone('us-east-1')}]
+              { id: "dev", stackBuilder: blueprint.clone('us-west-2').addOns(devBootstrapArgo)},
+              { id: "test", stackBuilder: blueprint.clone('us-east-2').addOns(testBootstrapArgo)},
+              { id: "prod", stackBuilder: blueprint.clone('us-east-1').addOns(prodBootstrapArgo)}]
       })
       .build(scope, id+'-stack', props);
   }
